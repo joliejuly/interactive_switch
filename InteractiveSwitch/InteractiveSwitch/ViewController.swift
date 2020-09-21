0d34b566b9ab5d1280e4c6248f2ddfe1a87ac8f4
Julia Nikitina <julianikitina.ios@gmail.com>
2020-09-21 17:40:00 +0300

    ++

---
+++ InteractiveSwitch/InteractiveSwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractiveSwitch
+//
+//  Created by Julia Nikitina on 21.09.2020.
+//  Copyright © 2020 Julia Nikitina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
